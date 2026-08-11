85e807aad06e38de508a2ebeea13259374284e68
Gabriel Boalento <gabrielboalento3004@gmail.com>
2026-08-11 08:18:25 -0300

    feat(ui): export product ui primitives

---
+++ src/components/ui/index.js
@@ -0,0 +1,4 @@
+export { Card, CardHeader, CardContent, CardFooter, cardClasses } from './Card'
+export { MetricCard } from './MetricCard'
+export { Panel } from './Panel'
+export { StatusBadge } from './StatusBadge'
