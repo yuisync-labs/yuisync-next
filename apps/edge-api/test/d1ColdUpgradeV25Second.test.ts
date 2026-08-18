42e18c3d3aa7c7ef1aed1225009e175c5dce2c94
Gabriel Boalento <gabrielboalento3004@gmail.com>
2026-08-18 15:26:23 -0300

    test(edge): certify second isolated v25 to v30 upgrade

---
+++ apps/edge-api/test/d1ColdUpgradeV25Second.test.ts
@@ -0,0 +1,17 @@
+import { env } from 'cloudflare:workers'
+import { describe, it } from 'vitest'
+
+import { runColdUpgradeV25 } from './d1ColdUpgradeV25Harness'
+
+type TestEnv = EdgeEnv & {
+  DB: D1Database
+  TEST_MIGRATIONS: Parameters<typeof import('cloudflare:test').applyD1Migrations>[1]
+}
+
+const testEnv = env as TestEnv
+
+describe('D1 cold upgrade proof 2/2', () => {
+  it('upgrades a second isolated v25 snapshot to v30', async () => {
+    await runColdUpgradeV25(testEnv.DB, testEnv.TEST_MIGRATIONS)
+  })
+})
